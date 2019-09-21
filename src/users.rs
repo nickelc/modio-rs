1c547aa1b9751d6bfb4185d13f685df5136fd052
Constantin Nickel <constantin.nickel@gmail.com>
2019-09-21 20:40:32 +0200

    Drop the `/users*` endpoints

--- src/users.rs
+++ src/users.rs
@@ -16,39 +16,6 @@ impl Users {
         Self { modio }
     }
 
-    /*
-    /// List all users registered on [mod.io](https:://mod.io).
-    ///
-    /// See [Filters and sorting](filters/index.html).
-    pub async fn list(&self, filter: &Filter) -> Result<List<User>> {
-        let mut uri = vec!["/users".into()];
-        let query = filter.to_query_string();
-        if !query.is_empty() {
-            uri.push(query);
-        }
-        let url = uri.join("?");
-        self.modio.get(&url).await
-    }
-
-    /// Provides a stream over all users registered on [mod.io](https:://mod.io).
-    ///
-    /// See [Filters and sorting](filters/index.html).
-    pub fn iter(&self, filter: &Filter) -> Stream<User> {
-        let mut uri = vec!["/users".into()];
-        let query = filter.to_query_string();
-        if !query.is_empty() {
-            uri.push(query);
-        }
-        self.modio.stream(&uri.join("?"))
-    }
-
-    /// Return a user by id
-    pub async fn get(&self, id: u32) -> Result<User> {
-        let url = format!("/users/{}", id);
-        self.modio.get(&url).await
-    }
-    */
-
     /// Return the user that is the original submitter of a resource. [required: token]
     pub async fn get_owner(self, resource: Resource) -> Result<User> {
         self.modio
@@ -57,47 +24,6 @@ impl Users {
             .send()
             .await
     }
-}
-
-/// Options used to filter user listings
-///
-/// # Filter parameters
-/// - Fulltext
-/// - Id
-/// - NameId
-/// - DateOnline
-/// - Username
-/// - Timezone
-/// - Language
-///
-/// # Sorting
-/// - Id
-/// - Username
-///
-/// See [modio docs](https://docs.mod.io/#get-all-users) for more information.
-///
-/// By default this returns up to `100` items. You can limit the result by using `limit` and
-/// `offset`.
-/// # Example
-/// ```
-/// use modio::filter::prelude::*;
-/// use modio::users::filters::Id;
-///
-/// let filter = Id::_in(vec![1, 2]).order_by(Id::desc());
-/// ```
-#[rustfmt::skip]
-pub mod filters {
-    #[doc(inline)]
-    pub use crate::filter::prelude::Fulltext;
-    #[doc(inline)]
-    pub use crate::filter::prelude::Id;
-    #[doc(inline)]
-    pub use crate::filter::prelude::NameId;
-
-    filter!(DateOnline, DATE_ONLINE, "date_online", Eq, NotEq, In, Cmp, OrderBy);
-    filter!(Username, USERNAME, "username", Eq, NotEq, In, Like, OrderBy);
-    filter!(Timezone, TIMEZONE, "timezone", Eq, NotEq, In, Like);
-    filter!(Language, LANGUAGE, "language", Eq, NotEq, In, Like);
 }
 
 #[derive(Clone, Copy)]
