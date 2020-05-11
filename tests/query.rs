af97d7d0f1540d7906816d81b2e5faffe9207e16
Constantin Nickel <constantin.nickel@gmail.com>
2020-05-11 17:59:50 +0200

    Add more `Query` tests

--- tests/query.rs
+++ tests/query.rs
@@ -33,6 +33,18 @@ fn create_empty_result() -> Server {
     server
 }
 
+fn create_first_page_only() -> Server {
+    let server = Server::run();
+
+    expect_requests!(
+        server,
+        query: not(contains(key("_offset"))),
+        body:  include_str!("fixtures/games-page1.json")
+    );
+
+    server
+}
+
 fn create_games_endpoint() -> Server {
     let server = Server::run();
 
@@ -58,14 +70,79 @@ fn create_games_endpoint() -> Server {
 }
 
 #[tokio::test]
+async fn empty_first() -> Result<()> {
+    env_logger::try_init().ok();
+    let server = create_empty_result();
+
+    let modio = Modio::host(server.url_str("/v1"), "foobar")?;
+    let first = modio.games().search(Default::default()).first().await?;
+
+    assert!(first.is_none());
+    Ok(())
+}
+
+#[tokio::test]
+async fn first() -> Result<()> {
+    env_logger::try_init().ok();
+    let server = create_first_page_only();
+
+    let modio = Modio::host(server.url_str("/v1"), "foobar")?;
+    let first = modio.games().search(Default::default()).first().await?;
+
+    assert!(first.is_some());
+    assert_eq!(1, first.unwrap().id);
+    Ok(())
+}
+
+#[tokio::test]
+async fn empty_first_page() -> Result<()> {
+    env_logger::try_init().ok();
+    let server = create_empty_result();
+
+    let modio = Modio::host(server.url_str("/v1"), "foobar")?;
+    let list = modio.games().search(Default::default()).first_page().await?;
+
+    assert!(list.is_empty());
+    Ok(())
+}
+
+#[tokio::test]
+async fn first_page() -> Result<()> {
+    env_logger::try_init().ok();
+    let server = create_first_page_only();
+
+    let modio = Modio::host(server.url_str("/v1"), "foobar")?;
+    let list = modio.games().search(Default::default()).first_page().await?;
+
+    assert_eq!(7, list.len());
+    assert_eq!(1, list[0].id);
+    assert_eq!(51, list[6].id);
+    Ok(())
+}
+
+#[tokio::test]
 async fn empty_collect() -> Result<()> {
     env_logger::try_init().ok();
     let server = create_empty_result();
 
     let modio = Modio::host(server.url_str("/v1"), "foobar")?;
     let list = modio.games().search(Default::default()).collect().await?;
-    assert!(list.is_empty());
 
+    assert!(list.is_empty());
+    Ok(())
+}
+
+#[tokio::test]
+async fn collect() -> Result<()> {
+    env_logger::try_init().ok();
+    let server = create_games_endpoint();
+
+    let modio = Modio::host(server.url_str("/v1"), "foobar")?;
+    let list = modio.games().search(Default::default()).collect().await?;
+
+    assert_eq!(31, list.len());
+    assert_eq!(1, list[0].id);
+    assert_eq!(296, list[30].id);
     Ok(())
 }
 
