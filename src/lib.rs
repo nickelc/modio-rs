07a1edd46b196b35fa6a0077ac7bb8e37ad1bea1
Constantin Nickel <constantin.nickel@gmail.com>
2022-09-19 12:34:33 +0200

    Always deny broken intra-doc links

--- src/lib.rs
+++ src/lib.rs
@@ -111,8 +111,8 @@
 //! ```
 #![doc(html_root_url = "https://docs.rs/modio/0.7.0")]
 #![deny(rust_2018_idioms)]
+#![deny(rustdoc::broken_intra_doc_links)]
 #![allow(clippy::upper_case_acronyms)]
-#![cfg_attr(docsrs, deny(broken_intra_doc_links))]
 
 #[macro_use]
 mod macros;
