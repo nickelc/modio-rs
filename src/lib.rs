420f96ad34dc10d264540330dddf6403e91e30c9
Constantin Nickel <constantin.nickel@gmail.com>
2023-09-22 05:12:57 +0200

    v0.8.2

--- src/lib.rs
+++ src/lib.rs
@@ -109,7 +109,7 @@
 //! #    Ok(())
 //! # }
 //! ```
-#![doc(html_root_url = "https://docs.rs/modio/0.8.1")]
+#![doc(html_root_url = "https://docs.rs/modio/0.8.2")]
 #![deny(rust_2018_idioms)]
 #![deny(rustdoc::broken_intra_doc_links)]
 #![allow(clippy::upper_case_acronyms)]
